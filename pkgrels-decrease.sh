5644198a32452addeeed147b2ce251eb51e8ba67
amnon <philm@chakra-project.org>
2010-08-06 23:36:00 +0000

    total rebuild of KDE SC 4.5.0 - new src tarballs - kdepim(-runtime) can be kept

--- pkgrels-decrease.sh
+++ pkgrels-decrease.sh
@@ -190,6 +190,37 @@ decrease_pkgrels()
 			status_done
 			done
 		;;
+
+		*)
+			title2 "Decreasing pkgrels"
+			for module in ${whattodo[*]}
+			do
+			status_start "$module"
+			pushd $module &>/dev/null
+				sed -i -e 's/\<pkgrel=2\>/pkgrel=1/g' PKGBUILD
+				sed -i -e 's/\<pkgrel=3\>/pkgrel=2/g' PKGBUILD
+				sed -i -e 's/\<pkgrel=4\>/pkgrel=3/g' PKGBUILD
+				sed -i -e 's/\<pkgrel=5\>/pkgrel=4/g' PKGBUILD
+				sed -i -e 's/\<pkgrel=6\>/pkgrel=5/g' PKGBUILD
+				sed -i -e 's/\<pkgrel=7\>/pkgrel=6/g' PKGBUILD
+				sed -i -e 's/\<pkgrel=8\>/pkgrel=7/g' PKGBUILD
+				sed -i -e 's/\<pkgrel=9\>/pkgrel=8/g' PKGBUILD
+				sed -i -e 's/\<pkgrel=10\>/pkgrel=9/g' PKGBUILD
+				sed -i -e 's/\<pkgrel=11\>/pkgrel=10/g' PKGBUILD
+				sed -i -e 's/\<pkgrel=12\>/pkgrel=11/g' PKGBUILD
+				sed -i -e 's/\<pkgrel=13\>/pkgrel=12/g' PKGBUILD
+				sed -i -e 's/\<pkgrel=14\>/pkgrel=13/g' PKGBUILD
+				sed -i -e 's/\<pkgrel=15\>/pkgrel=14/g' PKGBUILD
+				sed -i -e 's/\<pkgrel=16\>/pkgrel=15/g' PKGBUILD
+				sed -i -e 's/\<pkgrel=17\>/pkgrel=16/g' PKGBUILD
+				sed -i -e 's/\<pkgrel=18\>/pkgrel=17/g' PKGBUILD
+				sed -i -e 's/\<pkgrel=19\>/pkgrel=18/g' PKGBUILD
+				sed -i -e 's/\<pkgrel=20\>/pkgrel=19/g' PKGBUILD
+				sed -i -e 's/\<pkgrel=21\>/pkgrel=20/g' PKGBUILD
+			popd &>/dev/null
+			status_done
+			done
+		;;
 	esac         
 }
 
