a6ab9fc419a8706f28d15d90ca31e089a0fea261
philm <philm@chakra-project.org>
2010-12-26 10:30:56 +0100

    fix clean-workdir

--- clean-workdir.sh
+++ clean-workdir.sh
@@ -40,7 +40,7 @@ cleanup_src()
 	do
 		status_start "${module}"
 		pushd $module &>/dev/null
-		rm -rf pkg src dbg hdr *.pkg.* *log* &>/dev/null
+		rm -rf pkg src dbg hdr *.pkg.* *.log.* log &>/dev/null
 		popd &>/dev/null
 		status_done
 	done
