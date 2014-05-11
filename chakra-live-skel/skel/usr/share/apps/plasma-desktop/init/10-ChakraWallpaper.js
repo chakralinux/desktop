5e84ac42e2cddb0aaa7425ae1656509faf694578
AlmAck <gluca86@gmail.com>
2014-05-11 21:51:04 +0200

    removed activity name, it doesn't work correctly (the final activity name was empty)

--- chakra-live-skel/skel/usr/share/apps/plasma-desktop/init/10-ChakraWallpaper.js
+++ chakra-live-skel/skel/usr/share/apps/plasma-desktop/init/10-ChakraWallpaper.js
@@ -1,7 +1,6 @@
 /* Copyright 2014 Chakra Developer Team */
 
 var activity = activities()[0]
-activity.name = i18n("Chakra")
 activity.currentConfigGroup = new Array("Wallpaper", "image");
 activity.writeConfig("wallpaper", "/usr/share/wallpapers/descartes/");
 activity.writeConfig("userswallpaper", "/usr/share/wallpapers/descartes");
