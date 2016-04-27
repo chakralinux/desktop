c597bba45fac560b9675540582e3c3838bb97464
gnastyle <BM-2cTv4XtvC9UQ8TdFCfPNySv2omc8T6m3sv@bitmessage.ch>
2016-04-27 14:22:11 +0200

    freecol: update to 0.11.6

--- freecol/main.sh
+++ freecol/main.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-FC_PATH=/usr/share/java/freecol
-LOG_PATH=/tmp/freecol
+FC_PATH=/usr/share/freecol
 
-[ ! -d "$LOG_PATH" ] && install -dm755 $LOG_PATH
-cd $LOG_PATH
-java -Xmx256M -jar $FC_PATH/freecol.jar "$@" --freecol-data $FC_PATH/data \
-  &> ./messages.log
+java -Xmx512M -jar $FC_PATH/FreeCol.jar "$@" --freecol-data $FC_PATH/data --no-intro
