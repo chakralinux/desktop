fd1869a4c4adabf0a4fde243483144a9acf88b60
Chaoting Liu <brli@chakralinux.org>
2018-06-23 19:50:25 +0100

    ilibreoffice: fix build

---
+++ libreoffice/libreoffice.sh
@@ -0,0 +1,7 @@
+# to force a certain look'n feel
+
+#export SAL_USE_VCLPLUGIN=gen
+#export SAL_USE_VCLPLUGIN=kde4
+#export SAL_USE_VCLPLUGIN=gtk
+#export SAL_USE_VCLPLUGIN=gtk3
+#export SAL_USE_VCLPLUGIN=gtk3-kde5
